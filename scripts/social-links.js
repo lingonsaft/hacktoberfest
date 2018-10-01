3548fb8ce35ca79463e02942303be237a51287d6
Tanuj Vishnoi <vishnoitanuj@outlook.com>
2018-10-02 01:28:02 +0530

    Red Navbar of lingonsaft color added

--- scripts/social-links.js
+++ scripts/social-links.js
@@ -1,6 +1,5 @@
-let btn = document.getElementById('toggle-social-btn');
-
-btn.onclick = (e) => {
-    let footer = document.getElementById('footer');
-    footer.classList.toggle('footer-active');
+let btn = document.getElementById('toggle-social-btn')
+btn.onclick = (e) => { 
+    let footer = document.getElementById('footer')
+    footer.classList.toggle('active') 
 }
