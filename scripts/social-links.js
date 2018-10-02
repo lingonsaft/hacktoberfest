8fba615d2bf6fcf17d57c08757c2a1e283446434
Lina Johnson <linajohnson@flywheel.io>
2018-10-02 11:40:46 -0500

    Always display social icons, removed unnecessary js and references

--- scripts/social-links.js
+++
@@ -1,6 +0,0 @@
-let btn = document.getElementById('toggle-social-btn');
-
-btn.onclick = (e) => {
-    let footer = document.getElementById('footer');
-    footer.classList.toggle('footer-active');
-}
