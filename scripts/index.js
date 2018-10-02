6686d14f862e1a312009ddb38f8d1d8ce731dcb3
Chris Bigge <live.cbigge@gmail.com>
2018-10-02 14:12:47 -0500

    Added a Wild Wild West button to navbar that filters the whole page

--- scripts/index.js
+++ scripts/index.js
@@ -1,6 +1,11 @@
 let t = document.getElementById('twist').addEventListener('click', () => {
-    let cards = document.querySelectorAll('.card')    
+    let cards = document.querySelectorAll('.card')
     cards.forEach(card => {
         card.classList.toggle('spinCard');
     })
+}, false)
+let l = document.getElementById('cswitch').addEventListener('click', () => {
+    var sheet = document.createElement('style')
+    sheet.innerHTML = "body {filter: sepia(100%);}";
+    document.body.appendChild(sheet);
 }, false)
