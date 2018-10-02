b195ac917d44168176b5d4c6746f84cddee2a25e
Ad <adunreal@gmail.com>
2018-10-02 16:42:35 +0530

    Added ad-dan's contributions

--- scripts/contributors.js
+++ scripts/contributors.js
@@ -1,5 +1,7 @@
-let contributors = document.querySelectorAll('.card-text a')
+let contributors = document.querySelectorAll('.card-text a');
 contributors.forEach(con => {
-    console.log(con.href)
-    con.innerHTML += '<img style="width: 62px; margin-left: 12px" src="https://avatars.githubusercontent.com/'+ con.href.split('https://github.com/')[1] + '" />'
-})
+  console.log(con.href);
+  con.innerHTML += `<img style="width: 62px; margin-left: 12px" src="https://avatars.githubusercontent.com/'${
+    con.href.split('https://github.com/')[1]
+  }'" />`;
+});
