7396823fe3cff341d24eddcb4d01c19aa2c135e9
Jared Stevick <jstevick@gmail.com>
2018-10-02 16:26:46 -0400

    load script file and a neat console surprise

--- scripts/easter_egg.js
+++ scripts/easter_egg.js
@@ -3,3 +3,11 @@ document.addEventListener('keydown', function(event) {
     console.log("Äru svensk?");
   }
 });
+
+document.addEventListener('dblclick', function(event) {
+    if (event.target.id === "click-me") {
+        var size = Math.floor(Math.random() * 4) + 1;  
+        console.log("%cHAPPY \nHACKTOBERFEST!", "color: orange; font-size: 50px;");
+        console.log("%c  ", "background-image: url('https://www.placecage.com/" + size + "00/" + size + "00'); background-repeat: no-repeat; background-size: " + size + "00px " + size + "00px; font-size: " + size + "00px");
+    }
+});
