df217bb3738391374f8cd06353461e30fc8130ab
Evie Skinner <evie.skinner@gwle.onmicrosoft.com>
2018-10-02 15:35:49 +0100

    Added different font, styles, background and JS specifically for the cheese page

--- scripts/cheese.js
+++ scripts/cheese.js
@@ -1,1 +1,20 @@
-alert("Cheese works!");
+// alert("Cheese works!");
+
+
+function cheeseAppearance(){
+    var randomNum = Math.floor((Math.random() * 3) + 1);
+
+    switch (randomNum) {
+      case 0:
+        console.log("computer picks 0!");
+        break;
+      case 1:
+        console.log("computer picks 1!");
+        break;
+      case 2:
+        console.log("computer picks 2!");
+        break;
+      default:
+        console.log("computer picks summat else!");
+    }
+  }
