a4dc06c3d7ec57560ef47556f6fb8268cd2b7320
Evie Skinner <evie.skinner@gwle.onmicrosoft.com>
2018-10-02 16:27:30 +0100

    Added JS function to dynamically change background to photos of cheese when click for cheese is clicked

--- scripts/cheese.js
+++ scripts/cheese.js
@@ -1,21 +1,18 @@
-// alert("Cheese works!");
 
 
+// function to change the background of the <section> part of cheese.html when the user clicks 'click for cheese' btn
 function cheeseAppearance(){
-    //var cheeseButton = getElementById("click-for-cheese")
+    var jumbotron = document.getElementById("cheesetron");
     var randomNum = Math.floor((Math.random() * 3) + 1);
 
     switch (randomNum) {
-      case 0:
-        console.log("computer picks 0!");
-        break;
       case 1:
-        console.log("computer picks 1!");
+        jumbotron.className = 'cheesetron-1';
         break;
       case 2:
-        console.log("computer picks 2!");
+        jumbotron.className = 'cheesetron-2';
         break;
       default:
-        console.log("computer picks summat else!");
+        jumbotron.className = 'cheesetron-3';
     }
   }
