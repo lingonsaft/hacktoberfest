9f2f3296d9acfde2aaf0b7f7d9802e433835c891
Son Nguyen <43753960+sn149@users.noreply.github.com>
2018-10-03 18:04:50 +0700

    Update style for helpful-material. Fix socials footer & scroll to top. (#415)
    * Contributors dark style
    
    * update Contributors
    
    * Update Contributors
    
    * update style for helpful-material.html, fix socials footer & scroll to top

--- scripts/scroll.js
+++ scripts/scroll.js
@@ -1,13 +1,32 @@
-scrollFunction = () => {
-    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-        document.getElementById("myBtn").style.display = "block";
+window.onscroll = function() {scrollFunc()};
+function scrollFunc() {
+    if (document.body.scrollTop > 150 || document.documentElement.scrollTop > 150) {
+    document.getElementById('backToTop').style.display = "block";
     } else {
-        document.getElementById("myBtn").style.display = "none";
+    document.getElementById('backToTop').style.display = "none";
     }
 }
-
-window.onscroll = () => {scrollFunction()};
-topFunction = () => {
-    document.body.scrollTop = 0;
-    document.documentElement.scrollTop = 0;
+document.getElementById('backToTop').onclick = function() {
+    scrollTo(document.documentElement, 0, 1250);
 }
+function scrollTo(element, to, duration) {
+    var start = element.scrollTop,
+        change = to - start,
+        currentTime = 0,
+        increment = 20;
+    var animateScroll = function() {
+    currentTime += increment;
+    var val = Math.easeInOutQuad(currentTime, start, change, duration);
+    element.scrollTop = val;
+    if (currentTime < duration) {
+        setTimeout(animateScroll,increment);
+    }
+    };
+    animateScroll();
+}
+Math.easeInOutQuad = function(t,b,c,d) {
+    t /= d/2;
+    if (t < 1) return c/2*t*t + b;
+    t--;
+    return -c/2 * (t*(t-2) - 1) + b;
+};
