f60fec945548d05eccf2631d3b8161f50f4681b5
Carl Nettelbladt <carl.nettelbladt@aller.com>
2018-10-03 13:13:50 -0400

    Added a unified menu in JSON format

---
+++ scripts/menu.js
@@ -0,0 +1,28 @@
+async function fetchMenu() {
+    return fetch('./menu.json')
+        .then(r => r.json())
+}
+
+async function buildMenuHTML(obj = {}) {
+    var html = ''
+    var path = window.location.pathname.split('/')
+    var currentPage = path[path.length-1] === '' ? '/' : path[path.length-1]
+
+    Object.entries(obj).forEach(([key, item]) => {
+        let isCurrent = (currentPage === item.href ? true : false)
+        
+        html += '<li class="nav-item' + (isCurrent ? ' active' : '') + '">'
+        html += '<a class="nav-link" href="' + item.href + '"' + (item.id ? ' id="'+ item.id + '"': '') + '>' + item.text + '</a>'
+        html += '</li>'
+    })
+    return document.getElementById('menu').innerHTML = html
+}
+
+async function buildMenu() {
+    return fetchMenu()
+        .then(r => {
+            buildMenuHTML(r)
+        })
+}
+
+buildMenu()
