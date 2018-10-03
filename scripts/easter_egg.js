dbda1d012563a5669eaf4b71add24f021cbb03ca
Chris <mail@chris-heinrich.de>
2018-10-03 20:36:28 +0200

    add konami code as easter egg

--- scripts/easter_egg.js
+++ scripts/easter_egg.js
@@ -17,3 +17,43 @@ document.getElementById('invert-btn').onclick = (e) => {
     console.log('If you\'re using IE or Opera, you don\'t know what you are missing!');
   }
 }
+
+/*
+ * Konami code
+ *
+ */
+var allowedKeys = {
+  37: 'left',
+  38: 'up',
+  39: 'right',
+  40: 'down',
+  65: 'a',
+  66: 'b'
+};
+
+var konamiCode = ['up', 'up', 'down', 'down', 'left', 'right', 'left', 'right', 'b', 'a'];
+var codePosition = 0;
+
+document.addEventListener('keydown', function(e) {
+  var key = allowedKeys[e.keyCode];
+  var requiredKey = konamiCode[codePosition];
+
+  if (key === requiredKey) {
+    codePosition++;
+
+    if (codePosition === konamiCode.length) {
+      executeEasterEgg();
+      codePosition = 0;
+    }
+  } else {
+    codePosition = 0;
+  }
+});
+
+function executeEasterEgg() {
+  var html = document.createElement('div');
+  var image = '<div style="position:absolute;top:50%;left:50%;transform:translate(-50%,-50%);background-color:#ccc;padding:10px;"><h1 style="text-align:center;">You´re such a nerd!!!</h1><img src="https://i.giphy.com/media/4hnQDVKVARZ6w/giphy.webp"/></div>'
+
+  html.innerHTML = image;
+  document.body.appendChild(html);
+}
