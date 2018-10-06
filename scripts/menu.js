45b71b8f133a29725bc4d5d63af63e0e134929af
cnettelblad <carl.nettelblad@gmail.com>
2018-10-06 10:49:38 +0200

    Made the menu.js slightly object oriented and added myself as contributor (#842)
    * Added a unified menu in JSON format
    
    * Added the new menu to our html files
    
    * Hotfix - added menu.js to index.html
    
    * Added myself to contributors
    
    * Objectified the menu js to be a little friendlier to people who want to work on it

--- scripts/menu.js
+++ scripts/menu.js
@@ -1,28 +1,35 @@
-async function fetchMenu() {
-    return fetch('./menu.json')
-        .then(r => r.json())
-}
+class menu {
+    constructor() {
 
-async function buildMenuHTML(obj = {}) {
-    var html = ''
-    var path = window.location.pathname.split('/')
-    var currentPage = path[path.length-1] === '' ? '/' : path[path.length-1]
+    }
 
-    Object.entries(obj).forEach(([key, item]) => {
-        let isCurrent = (currentPage === item.href ? true : false)
-        
-        html += '<li class="nav-item' + (isCurrent ? ' active' : '') + '">'
-        html += '<a class="nav-link" href="' + item.href + '"' + (item.id ? ' id="'+ item.id + '"': '') + '>' + item.text + '</a>'
-        html += '</li>'
-    })
-    return document.getElementById('menu').innerHTML = html
-}
+    fetchMenu() {
+        return fetch('./menu.json')
+            .then(r => r.json())
+    }
 
-async function buildMenu() {
-    return fetchMenu()
-        .then(r => {
-            buildMenuHTML(r)
+    buildMenuHTML(obj = {}) {
+        var html = ''
+        var path = window.location.pathname.split('/')
+        var currentPage = path[path.length-1] === '' ? '/' : path[path.length-1]
+
+        Object.entries(obj).forEach(([key, item]) => {
+            let isCurrent = (currentPage === item.href ? true : false)
+            
+            html += '<li class="nav-item' + (isCurrent ? ' active' : '') + '">'
+            html += '<a class="nav-link" href="' + item.href + '"' + (item.id ? ' id="'+ item.id + '"': '') + '>' + item.text + '</a>'
+            html += '</li>'
         })
+        return document.getElementById('menu').innerHTML = html
+    }
+
+    build() {
+        return this.fetchMenu()
+            .then(r => {
+                this.buildMenuHTML(r)
+            })
+    }
 }
 
-buildMenu()
+let m = new menu()
+m.build()
