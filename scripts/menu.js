3a6d630528a0c944b55ce376dd12136f7a63213d
Ramya Praneetha <ramya.praneetha04@gmail.com>
2018-10-06 16:00:03 +0530

    Declutter navbar

--- scripts/menu.js
+++ scripts/menu.js
@@ -9,11 +9,25 @@ async function buildMenuHTML(obj = {}) {
     var currentPage = path[path.length-1] === '' ? '/' : path[path.length-1]
 
     Object.entries(obj).forEach(([key, item]) => {
-        let isCurrent = (currentPage === item.href ? true : false)
-        
-        html += '<li class="nav-item' + (isCurrent ? ' active' : '') + '">'
-        html += '<a class="nav-link" href="' + item.href + '"' + (item.id ? ' id="'+ item.id + '"': '') + '>' + item.text + '</a>'
-        html += '</li>'
+        if(key == 'Others') {
+          html += '<li class="dropdown">'
+          html += '<a class="nav-link nested-dropdown" href="#" id="Others"> Others </a>'
+          html += '<div class="dropdown-content">'
+          Object.entries(item).forEach(([key, item]) => {
+            let isCurrent = (currentPage === item.href ? true : false)
+
+            html += '<div class="nav-item' + (isCurrent ? ' active' : '') + '">'
+            html += '<a class="nav-link" href="' + item.href + '"' + (item.id ? ' id="'+ item.id + '"': '') + '>' + item.text + '</a>'
+            html += '</div>'
+          })
+          html += '</div>'
+        } else {
+          let isCurrent = (currentPage === item.href ? true : false)
+
+          html += '<li class="nav-item' + (isCurrent ? ' active' : '') + '">'
+          html += '<a class="nav-link" href="' + item.href + '"' + (item.id ? ' id="'+ item.id + '"': '') + '>' + item.text + '</a>'
+          html += '</li>'
+        }
     })
     return document.getElementById('menu').innerHTML = html
 }
