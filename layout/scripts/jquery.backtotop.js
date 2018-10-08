d4a25214b7a85cb861f7aa4681e1d3765ac5622a
Karan Badalia <karan.badalia.met17@itbhu.ac.in>
2018-10-09 00:43:14 +0530

    Added website

---
+++ layout/scripts/jquery.backtotop.js
@@ -0,0 +1,21 @@
+/*
+Template Name: Mudcappro
+Author: <a href="http://www.os-templates.com/">OS Templates</a>
+Author URI: http://www.os-templates.com/
+Licence: Free to use under our free template licence terms
+Licence URI: http://www.os-templates.com/template-terms
+File: Back to Top JS
+*/
+
+jQuery("#backtotop").click(function () {
+    jQuery("body,html").animate({
+        scrollTop: 0
+    }, 600);
+});
+jQuery(window).scroll(function () {
+    if (jQuery(window).scrollTop() > 150) {
+        jQuery("#backtotop").addClass("visible");
+    } else {
+        jQuery("#backtotop").removeClass("visible");
+    }
+});
