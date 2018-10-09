ccd7eca88870d6e0a8993852ca5a2d44b6a9da2d
Maciej Bembenista <maciej.bembenista@gmail.com>
2018-10-09 14:41:00 +0200

    Code style changes to cheese.js

--- scripts/cheese.js
+++ scripts/cheese.js
@@ -1,23 +1,25 @@
-let t = document.getElementById('twist').addEventListener('click', () => {
-    let cards = document.querySelectorAll('.spin')    
-    cards.forEach(card => {
-        card.classList.toggle('spinCard');
-    })
-}, false)
+let twistHandler = document.getElementById('twist').addEventListener('click', () => {
+    let cards = document.querySelectorAll('.spin');
+    
+    cards.forEach(card => card.classList.toggle('spinCard'));
+}, false);
 
 // function to change the background of the <section> part of cheese.html when the user clicks 'click for cheese' btn
 function cheeseAppearance(){
-    var jumbotron = document.getElementById("cheesetron");
-    var randomNum = Math.floor((Math.random() * 3) + 1);
+    const jumbotron = document.getElementById('cheesetron');
+    const randomNum = Math.floor((Math.random() * 3) + 1);
 
     switch (randomNum) {
-      case 1:
+      case 1: {
         jumbotron.className = 'cheesetron-1';
         break;
-      case 2:
+      }
+      case 2: {
         jumbotron.className = 'cheesetron-2';
         break;
-      default:
+      }
+      default: {
         jumbotron.className = 'cheesetron-3';
+      }
     }
   }
