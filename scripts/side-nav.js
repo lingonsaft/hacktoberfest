8f2df4d10ac9450f2573701e8ea4000b7400f16e
Becky <becky_w5@hotmail.co.uk>
2018-10-10 23:18:21 +0100

    added smooth scrolling to side nav

--- scripts/side-nav.js
+++ scripts/side-nav.js
@@ -5,3 +5,15 @@ $('.side-navigtion__item').on('click', function(){
         $clickedNav.addClass('active');
     }
 });
+
+$("a.side-navigtion__item").on('click', function (event) {
+    if (this.hash !== "") {
+        event.preventDefault();
+        var hash = this.hash;
+        $('html, body').animate({
+            scrollTop: $(hash).offset().top 
+        }, 800, function () {
+            window.location.hash = hash;
+        });
+    } 
+});
