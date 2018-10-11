6f2fecc2d64b3b242c00a8d5f3013d7ef6dafa62
Deadpool1994 <Deadpool1994@users.noreply.github.com>
2018-10-11 14:02:44 +0530

    Insertion sort (#1508)
    * Added a new anime in list #hacktoberfest-2018 deadpool1994
    
    * Added a new sorting page
    
    * add a message

--- scripts/index.js
+++ scripts/index.js
@@ -10,16 +10,3 @@ let l = document.getElementById('love').addEventListener('click', () => {
   document.querySelector("#header h1").innerHTML = "Broccoli Egg";
   body.classList.toggle("broccoli-green");
 }, false)
-
-//Adds a heart above and below the header when the heart icon is clicked (MAX 10 because October :P)
-let h = document.getElementById('heart').addEventListener('click', () => {
-    let bars = document.querySelectorAll('#heart-bar');
-    bars.forEach(bar => {
-        if (bar.childElementCount < 10) {
-            var heart = document.createElement("img");
-            heart.innerHTML = '<img src="./images/heart.png" alt="heart icon" width="30px">';
-            bar.appendChild(heart);
-        }
-    })
-}, false)
-
