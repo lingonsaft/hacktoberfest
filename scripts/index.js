0f10648c5316b7a219ae80ffb0ff04e1b0e0588c
isholaomotayo <isholaomotayo@yahoo.com>
2018-10-12 15:45:49 +0100

    Update index.js

--- scripts/index.js
+++ scripts/index.js
@@ -10,3 +10,54 @@ let l = document.getElementById('love').addEventListener('click', () => {
   document.querySelector("#header h1").innerHTML = "Broccoli Egg";
   body.classList.toggle("broccoli-green");
 }, false)
+
+Cofetti code
+
+for (var i = 0; i < 250; i++) {
+  create(i);
+}
+
+function create(i) {
+  var width = Math.random() * 8;
+  var height = width * 0.4;
+  var colourIdx = Math.ceil(Math.random() * 3);
+  var colour = "red";
+  switch(colourIdx) {
+    case 1:
+      colour = "yellow";
+      break;
+    case 2:
+      colour = "blue";
+      break;
+    default:
+      colour = "red";
+  }
+  $('<div class="confetti-'+i+' '+colour+'"></div>').css({
+    "width" : width+"px",
+    "height" : height+"px",
+    "top" : -Math.random()*20+"%",
+    "left" : Math.random()*100+"%",
+    "opacity" : Math.random()+0.5,
+    "transform" : "rotate("+Math.random()*360+"deg)"
+  }).appendTo('.wrapper');  
+  
+  drop(i);
+}
+
+function drop(x) {
+  $('.confetti-'+x).animate({
+    top: "100%",
+    left: "+="+Math.random()*15+"%"
+  }, Math.random()*3000 + 3000, function() {
+    reset(x);
+  });
+}
+
+function reset(x) {
+  $('.confetti-'+x).animate({
+    "top" : -Math.random()*20+"%",
+    "left" : "-="+Math.random()*15+"%"
+  }, 0, function() {
+    drop(x);             
+  });
+}
