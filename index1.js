1aabaff3b27e218feb615f8a35adc8ff3e51f503
sarthakahuja11 <sarthakahuja11@gmail.com>
2018-10-29 22:51:00 +0530

    Added index file

---
+++ index1.js
@@ -0,0 +1,301 @@
+/* This code has been generated from your interaction model by skillinator.io
+
+/* eslint-disable  func-names */
+/* eslint quote-props: ["error", "consistent"]*/
+
+// There are three sections, Text Strings, Skill Code, and Helper Function(s).
+// You can copy and paste the contents as the code for a new Lambda function, using the alexa-skill-kit-sdk-factskill template.
+// This code includes helper functions for compatibility with versions of the SDK prior to 1.0.9, which includes the dialog directives.
+
+
+
+ // 1. Text strings =====================================================================================================
+ //    Modify these strings and messages to change the behavior of your Lambda function
+
+
+let speechOutput;
+let reprompt;
+let welcomeOutput = "Welcome to Auto Booking Service! Tell me where you wish to go";
+let welcomeReprompt = "sample re-prompt text";
+// 2. Skill Code =======================================================================================================
+"use strict";
+const Alexa = require('alexa-sdk');
+const APP_ID = undefined;  // TODO replace with your app ID (OPTIONAL).
+speechOutput = '';
+const handlers = {
+	'LaunchRequest': function () {
+		this.emit(':ask', welcomeOutput, welcomeReprompt);
+	},
+	'AMAZON.HelpIntent': function () {
+		speechOutput = 'Economy auto is normal standard auto; Deluxe Auto has a welcome drink, comfortable seats, more leg space while Multi-Carrier can carry upto 5 people'
+		reprompt = '';
+		this.emit(':ask', speechOutput, reprompt);
+	},
+   'AMAZON.CancelIntent': function () {
+		speechOutput = 'No worries! We were happy to serve you! Hope to see you soon!';
+		this.emit(':tell', speechOutput);
+	},
+   'AMAZON.StopIntent': function () {
+		speechOutput = 'Stopped'
+   },
+   'SessionEndedRequest': function () {
+		speechOutput = '';
+		this.emit(':saveState', true);//uncomment to save attributes to db on session end
+		this.emit(':tell', speechOutput);
+   },
+	'BookAuto': function () {
+		speechOutput = '';
+
+		//any intent slot variables are listed here for convenience
+
+		let locationSlotRaw = this.event.request.intent.slots.location.value;
+		console.log(locationSlotRaw);
+		let locationSlot = resolveCanonical(this.event.request.intent.slots.location);
+		console.log(locationSlot);
+
+		//Your custom intent handling goes here
+		speechOutput = "Ok, great! Tell us where you currently are?";
+		this.emit(":ask", speechOutput, speechOutput);
+		//locationSlot=this.attributes['BookAuto'];
+		//speechOutput = "Your destination is" + locationSlot;
+		//this.emit(":ask", speechOutput, speechOutput);
+		//say='Your destination is ' + locationSlot;
+    },
+	'SourceAuto': function () {
+		speechOutput = '';
+
+		//any intent slot variables are listed here for convenience
+
+		let locationSlotRaw = this.event.request.intent.slots.location.value;
+		console.log(locationSlotRaw);
+		let locationSlot = resolveCanonical(this.event.request.intent.slots.location);
+		console.log(locationSlot);
+
+		//Your custom intent handling goes here
+		speechOutput = "Ohh sounds good! Finally let us know your auto type from economy, deluxe and multicarrier. Say help incase you wish to know more!";
+		this.emit(":ask", speechOutput, speechOutput);
+    },
+	'AutoType': function () {
+		speechOutput = '';
+
+		//any intent slot variables are listed here for convenience
+
+		let autoSlotRaw = this.event.request.intent.slots.auto.value;
+		console.log(autoSlotRaw);
+		let autoSlot = resolveCanonical(this.event.request.intent.slots.auto);
+		console.log(autoSlot);
+        
+		//Your custom intent handling goes here
+		//x=Math.random();
+		speechOutput = "Everything seems set, your Auto number is 2324. The auto should reach in 5 mins and the estimated fare is 40. Incase to cancel the trip please say cancel"
+		
+		this.emit(":ask", speechOutput, speechOutput);
+    },
+	'FareCalculator': function () {
+		speechOutput = '';
+
+		//any intent slot variables are listed here for convenience
+
+
+		//Your custom intent handling goes here
+		speechOutput = "The total fare is Rs.40";
+		this.emit(":ask", speechOutput, speechOutput);
+    },	
+	'Unhandled': function () {
+        speechOutput = "The skill didn't quite understand what you wanted.  Do you want to try something else?";
+        this.emit(':ask', speechOutput, speechOutput);
+    }
+};
+
+exports.handler = (event, context) => {
+    const alexa = Alexa.handler(event, context);
+    alexa.appId = APP_ID;
+    // To enable string internationalization (i18n) features, set a resources object.
+    //alexa.resources = languageStrings;
+    alexa.registerHandlers(handlers);
+	alexa.dynamoDBTableName = 'DYNAMODB_TABLE_NAME'; //uncomment this line to save attributes to DB
+    alexa.execute();
+};
+
+//    END of Intent Handlers {} ========================================================================================
+// 3. Helper Function  =================================================================================================
+
+function resolveCanonical(slot){
+	//this function looks at the entity resolution part of request and returns the slot value if a synonyms is provided
+	let canonical;
+    try{
+		canonical = slot.resolutions.resolutionsPerAuthority[0].values[0].value.name;
+	}catch(err){
+	    console.log(err.message);
+	    canonical = slot.value;
+	};
+	return canonical;
+};
+
+function delegateSlotCollection(){
+  console.log("in delegateSlotCollection");
+  console.log("current dialogState: "+this.event.request.dialogState);
+    if (this.event.request.dialogState === "STARTED") {
+      console.log("in Beginning");
+	  let updatedIntent= null;
+	  // updatedIntent=this.event.request.intent;
+      //optionally pre-fill slots: update the intent object with slot values for which
+      //you have defaults, then return Dialog.Delegate with this updated intent
+      // in the updatedIntent property
+      //this.emit(":delegate", updatedIntent); //uncomment this is using ASK SDK 1.0.9 or newer
+	  
+	  //this code is necessary if using ASK SDK versions prior to 1.0.9 
+	  if(this.isOverridden()) {
+			return;
+		}
+		this.handler.response = buildSpeechletResponse({
+			sessionAttributes: this.attributes,
+			directives: getDialogDirectives('Dialog.Delegate', updatedIntent, null),
+			shouldEndSession: false
+		});
+		this.emit(':responseReady', updatedIntent);
+		
+    } else if (this.event.request.dialogState !== "COMPLETED") {
+      console.log("in not completed");
+      // return a Dialog.Delegate directive with no updatedIntent property.
+      //this.emit(":delegate"); //uncomment this is using ASK SDK 1.0.9 or newer
+	  
+	  //this code necessary is using ASK SDK versions prior to 1.0.9
+		if(this.isOverridden()) {
+			return;
+		}
+		this.handler.response = buildSpeechletResponse({
+			sessionAttributes: this.attributes,
+			directives: getDialogDirectives('Dialog.Delegate', null, null),
+			shouldEndSession: false
+		});
+		this.emit(':responseReady');
+		
+    } else {
+      console.log("in completed");
+      console.log("returning: "+ JSON.stringify(this.event.request.intent));
+      // Dialog is now complete and all required slots should be filled,
+      // so call your normal intent handler.
+      return this.event.request.intent;
+    }
+}
+
+
+function randomPhrase(array) {
+    // the argument is an array [] of words or phrases
+    let i = 0;
+    i = Math.floor(Math.random() * array.length);
+    return(array[i]);
+}
+function isSlotValid(request, slotName){
+        let slot = request.intent.slots[slotName];
+        //console.log("request = "+JSON.stringify(request)); //uncomment if you want to see the request
+        let slotValue;
+
+        //if we have a slot, get the text and store it into speechOutput
+        if (slot && slot.value) {
+            //we have a value in the slot
+            slotValue = slot.value.toLowerCase();
+            return slotValue;
+        } else {
+            //we didn't get a value in the slot.
+            return false;
+        }
+}
+
+//These functions are here to allow dialog directives to work with SDK versions prior to 1.0.9
+//will be removed once Lambda templates are updated with the latest SDK
+
+function createSpeechObject(optionsParam) {
+    if (optionsParam && optionsParam.type === 'SSML') {
+        return {
+            type: optionsParam.type,
+            ssml: optionsParam['speech']
+        };
+    } else {
+        return {
+            type: optionsParam.type || 'PlainText',
+            text: optionsParam['speech'] || optionsParam
+        };
+    }
+}
+
+function buildSpeechletResponse(options) {
+    let alexaResponse = {
+        shouldEndSession: options.shouldEndSession
+    };
+
+    if (options.output) {
+        alexaResponse.outputSpeech = createSpeechObject(options.output);
+    }
+
+    if (options.reprompt) {
+        alexaResponse.reprompt = {
+            outputSpeech: createSpeechObject(options.reprompt)
+        };
+    }
+
+    if (options.directives) {
+        alexaResponse.directives = options.directives;
+    }
+
+    if (options.cardTitle && options.cardContent) {
+        alexaResponse.card = {
+            type: 'Simple',
+            title: options.cardTitle,
+            content: options.cardContent
+        };
+
+        if(options.cardImage && (options.cardImage.smallImageUrl || options.cardImage.largeImageUrl)) {
+            alexaResponse.card.type = 'Standard';
+            alexaResponse.card['image'] = {};
+
+            delete alexaResponse.card.content;
+            alexaResponse.card.text = options.cardContent;
+
+            if(options.cardImage.smallImageUrl) {
+                alexaResponse.card.image['smallImageUrl'] = options.cardImage.smallImageUrl;
+            }
+
+            if(options.cardImage.largeImageUrl) {
+                alexaResponse.card.image['largeImageUrl'] = options.cardImage.largeImageUrl;
+            }
+        }
+    } else if (options.cardType === 'LinkAccount') {
+        alexaResponse.card = {
+            type: 'LinkAccount'
+        };
+    } else if (options.cardType === 'AskForPermissionsConsent') {
+        alexaResponse.card = {
+            type: 'AskForPermissionsConsent',
+            permissions: options.permissions
+        };
+    }
+
+    let returnResult = {
+        version: '1.0',
+        response: alexaResponse
+    };
+
+    if (options.sessionAttributes) {
+        returnResult.sessionAttributes = options.sessionAttributes;
+    }
+    return returnResult;
+}
+
+function getDialogDirectives(dialogType, updatedIntent, slotName) {
+    let directive = {
+        type: dialogType
+    };
+
+    if (dialogType === 'Dialog.ElicitSlot') {
+        directive.slotToElicit = slotName;
+    } else if (dialogType === 'Dialog.ConfirmSlot') {
+        directive.slotToConfirm = slotName;
+    }
+
+    if (updatedIntent) {
+        directive.updatedIntent = updatedIntent;
+    }
+    return [directive];
+}
